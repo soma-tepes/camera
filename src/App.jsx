30aa64863527c99d420eda32c00f6e849225dbcf
Erika Vazquez Perez <evazquezperez@local.jaeoregon.com>
2023-03-28 14:33:34 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './assets/components/Header'
+import Navbar from './assets/components/Navbar'
+
+function App() {
+
+  
+ 
+
+
+  return (
+    <div className="App">
+     
+     <Navbar/>
+     <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import imagen from "../images/logo.webp"
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+    let phrase = `EMPRESA QUE OFRECE SUS SERVICIOS DE INSTALACIÓN Y MANTENIMIENTO EN SISTEMAS DE SEGURIDAD`
+     const nav = ['Title','Contact','Mision']
+
+    return (
+        <div className='navbar'>
+
+            <div className='nav_tex_img'>
+
+                <div className='nav1'>
+                    <img src={imagen} alt="" />
+                </div>
+                <div>
+                    <a href="#">Dayner's Efficiency</a>
+
+                </div>
+            </div>
+
+
+
+            <div className='nav_list'>
+              
+                <ul>
+                    <li>{nav[0]}</li>
+                    <li>{nav[1]}</li>
+                    <li>{nav[2]}</li>
+                </ul>
+            </div>
+
+           
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Navbar
